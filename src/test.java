da1a50d13208e8055561644dcd643e0b1f912bbe
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-02-01 16:12:57 +0100

    Modif

--- src/test.java
+++ src/test.java
@@ -5,7 +5,7 @@ import com.mysql.*;
 public class test {
 	public static void main(String[] args) throws SQLException {
 		Manager manager= new Manager();
-		System.out.println(manager.loginUser("alexandre@hotmail.fr", "1234"));
+		manager.loginUser("alexandre@hotmail.fr", "1234");
 	}
 
 }
